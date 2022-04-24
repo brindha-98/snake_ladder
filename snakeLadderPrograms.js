1318d5ec950cf3a6d53a49d7dbdd28ebf5f09678
brindha-98 <brindhaselva98@gmail.com>
2022-04-24 11:49:37 +0530

    Initialize checkposition variable

--- snakeLadderPrograms.js
+++ snakeLadderPrograms.js
@@ -2,6 +2,7 @@ var player_1 = 0;
 var b = Math.floor((Math.random() * 6) + 1);
 var ladder = [17, 29, 34, 60, 82];
 var snake = [20, 32, 55, 63, 92];
+var checkposition;
 player_1 = player_1 + b;
 for (var i = 0; i < ladder.length; i++) {
     if (player_1 == ladder[i]) {
