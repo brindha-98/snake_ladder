3c66f1ea796acd78cd043e70a578033706a897c9
brindha-98 <brindhaselva98@gmail.com>
2022-04-24 12:13:27 +0530

    Check snake_ladder board rules & regulation

--- snakeLadderPrograms.js
+++ snakeLadderPrograms.js
@@ -1,9 +1,10 @@
 var player_1 = 0;
-var b = Math.floor((Math.random() * 6) + 1);
+var diceValue = Math.floor((Math.random() * 6) + 1);
 var ladder = [17, 29, 34, 60, 82];
 var snake = [20, 32, 55, 63, 92];
-var checkposition;
-player_1 = player_1 + b;
+var checkPosition;
+var boardSize = 100;
+player_1 = player_1 + diceValue;
 for (var i = 0; i < ladder.length; i++) {
     if (player_1 == ladder[i]) {
         player_1 = player_1 + 7;
@@ -12,5 +13,11 @@ for (var i = 0; i < ladder.length; i++) {
 for (var i = 0; i < snake.length; i++) {
     if (player_1 == snake[i]) {
         player_1 = player_1 - 10;
+    }
+}
+if (player_1 >= 94) {
+    checkPosition = boardSize - player_1;
+    if (checkPosition <= diceValue) {
+        player_1 = player_1 + diceValue;
     }
 }
