25fa4b184fa1d1a3fad9b7d89143e3d1eb6c02b8
brindha-98 <brindhaselva98@gmail.com>
2022-04-24 11:45:39 +0530

    Implementation of Ladder and Snake functionality

--- snakeLadderPrograms.js
+++ snakeLadderPrograms.js
@@ -1,2 +1,15 @@
 var player_1 = 0;
 var b = Math.floor((Math.random() * 6) + 1);
+var ladder = [17, 29, 34, 60, 82];
+var snake = [20, 32, 55, 63, 92];
+player_1 = player_1 + b;
+for (var i = 0; i < ladder.length; i++) {
+    if (player_1 == ladder[i]) {
+        player_1 = player_1 + 7;
+    }
+}
+for (var i = 0; i < snake.length; i++) {
+    if (player_1 == snake[i]) {
+        player_1 = player_1 - 10;
+    }
+}
