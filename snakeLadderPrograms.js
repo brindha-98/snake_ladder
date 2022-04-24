ddb6c825f4a841f38e7ee721e4b4e592b5e51be5
brindha-98 <brindhaselva98@gmail.com>
2022-04-24 12:59:54 +0530

    Implementation of loop execution

--- snakeLadderPrograms.js
+++ snakeLadderPrograms.js
@@ -1,23 +1,30 @@
 var player_1 = 0;
-var diceValue = Math.floor((Math.random() * 6) + 1);
+var diceValue;
 var ladder = [17, 29, 34, 60, 82];
 var snake = [20, 32, 55, 63, 92];
 var checkPosition;
 var boardSize = 100;
-player_1 = player_1 + diceValue;
-for (var i = 0; i < ladder.length; i++) {
-    if (player_1 == ladder[i]) {
-        player_1 = player_1 + 7;
+for (var i = player_1; player_1 < boardSize; i++) {
+    diceValue = Math.floor((Math.random() * 6) + 1);
+    if (player_1 >= 94) {
+        checkPosition = boardSize - player_1;
+        console.log(diceValue, player_1, checkPosition);
+        if (diceValue <= checkPosition) {
+            player_1 = player_1 + diceValue;
+        }
     }
-}
-for (var i = 0; i < snake.length; i++) {
-    if (player_1 == snake[i]) {
-        player_1 = player_1 - 10;
-    }
-}
-if (player_1 >= 94) {
-    checkPosition = boardSize - player_1;
-    if (checkPosition <= diceValue) {
+    else {
         player_1 = player_1 + diceValue;
     }
+    for (var i = 0; i < ladder.length; i++) {
+        if (player_1 == ladder[i]) {
+            player_1 = player_1 + 7;
+        }
+    }
+    for (var i = 0; i < snake.length; i++) {
+        if (player_1 == snake[i]) {
+            player_1 = player_1 - 10;
+        }
+    }
+    console.log(diceValue, player_1);
 }
